89df8c183967e767245d896b9b5c20c6660e4f8a
Adegbemigun Adedayo <117754792+adedayoprcs@users.noreply.github.com>
2023-05-24 22:02:03 +0100

    created my first POST request

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
 
 app = Flask(__name__) 
 
-@app.route("/")
-def index():
-    return "Hello World!"
+books = { "1" :{
+        "book_id": "1",
+        "name": "A Game of Thrones.",
+        "author": "George R. R. Martin"}} 
+
+@app.route('/api/books', methods=['GET', 'POST'])
+def get_books():
+    if request.method == 'GET':
+        return make_response(jsonify(books), 200)
+    elif request.method == 'POST':
+        content = request.json
+        book_id = content['book_id']
+
+        books[book_id] = content
+
+        book_obj = books.get(book_id, {})
+        return make_response(jsonify(book_obj, 201))
+       
+
+
+         
+
+#@app.route("/")
+#def index():
+    #return "Hello World!"
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000, debug=True)
